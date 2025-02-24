23021673fe27c7cd2e3d8aeff4d21f7bf3834b0f
Samriddhi Tripathi <samriddhi.tripathi2711@gmail.com>
2025-02-24 14:24:03 +0530

    Updated Main file

--- DiagnoCare_chatbot/main.py
+++ DiagnoCare_chatbot/main.py
@@ -1,12 +1,15 @@
-#!/bin/env/python3
+
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui.mainwindow import MyHealthCareBot
 import qdarkstyle
 
+def main():
+    app = QApplication(sys.argv)
+    bot_window = MyHealthCareBot()
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    bot_window.show()
+    sys.exit(app.exec_())
+
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	window = MyHealthCareBot()
-	app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-	window.show()
-	sys.exit(app.exec_())
+    main()
